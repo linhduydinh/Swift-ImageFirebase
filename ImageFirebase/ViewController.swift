17448ddb0db3386756ef461df36f59fdb3d78c6e
Linh Dinh <linhduydinh@gmail.com>
2019-03-22 10:30:30 +0700

    Initial Commit

---
+++ ImageFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFirebase
+//
+//  Created by Linh Dinh on 3/22/19.
+//  Copyright © 2019 Linh Dinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
